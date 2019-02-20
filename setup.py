d705bfdc2ab6bc536565d5745fc21e6634121e70
reichholf.martin@gmail.com <reichholf.martin@gmail.com>
2019-02-20 15:24:03 +0100

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup, Extension
+
+test_module = Extension('pkg.test_ext',
+                        sources=['src/test_ext/test_ext.cpp'],
+                        include_dirs=['/usr/local/include'],
+                        library_dirs=['/usr/local/lib/boost'],
+                        runtime_library_dirs=['/usr/local/lib/boost'],
+                        libraries=['boost_python'])
+
+setup(name='Snake',
+      version='0.1',
+      description='Snake',
+      package_dir={'': 'src'},
+      packages=['pkg'],
+      ext_modules=[test_module])
