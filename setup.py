b498c09e7d738a5e824ee9c74fee4fb861a82759
reichholf.martin@gmail.com <reichholf.martin@gmail.com>
2019-02-20 19:20:21 +0100

    added cpp source

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from distutils.core import setup, Extension
+from distutils.core import setup, Extension 
 import sys
 
-test_module = Extension('pkg.test_ext',
-                        sources=['src/test_ext/test_ext.cpp'],
+_C = Extension('Snake._C',
+                        sources=['src/_C/_C.cpp'],
                         include_dirs=['/usr/local/include'],
                         library_dirs=['/usr/local/lib/boost'],
                         runtime_library_dirs=['/usr/local/lib/boost'],
-                        libraries=['boost_python3'])
+                        libraries=['boost_python3', 'boost_numpy3'])
 
 setup(name='Snake',
       version='0.1',
       description='Snake',
       package_dir={'': 'src'},
-      packages=['pkg'],
-      ext_modules=[test_module])
+      packages=['Snake'],
+      ext_modules=[_C])
